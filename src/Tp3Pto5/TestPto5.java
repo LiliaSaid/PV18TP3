8ab4e303b55893d177d2c4ccc735cfddcd83fcb9
LILI-PC <LILI-PC@DESKTOP-5ELCB6R>
2018-04-18 23:17:19 -0300

    array list de circulos completo--Said

--- src/Tp3Pto5/TestPto5.java
+++ src/Tp3Pto5/TestPto5.java
@@ -2,62 +2,43 @@
 package Tp3Pto5;
 
 //import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 public class TestPto5 {
     
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);       
-        GestorCirculos gc = new GestorCirculos();
-        
+        ArrayList<Circulo> circulo = new ArrayList<Circulo>();
+        double ra;
         int op;
             
             do{
-                            //       Circulo c1 = new Circulo();    
+                Circulo c1 = new Circulo();    
                 System.out.println("---seleccione opcion ingresando el numero de opcion---");
                 System.out.println("1) Agregar circulo ");
                 System.out.println("2) salir");
                 op=sc.nextInt();
                 if(op==1){
                     System.out.println("Ingrese radio ");
-                  //  gc.agregarCirculo(c1); = sc.nextDouble();
- 
-                    //gc.agregarCirculo(c1);
-                }
-                
-            }while(op==2);
-       
-      
+                    ra=sc.nextDouble();
+                    c1.setRadio(ra);
+                    circulo.add(c1);
+                    }
+            }while(op!=2);
+        Iterator<Circulo> it = circulo.iterator();    
+              
+         for(Circulo cir: circulo){//muestra los datos de array list falta colocar la ubicacion
             
-        int cant=gc.getCirculos().size();
-
-        //for (int i = 0; i < cant; i++) {
-          //System.out.println(gc.getCirculos(i);
-            
+             System.out.println("Radio ingresado: "+cir.getRadio());
+             System.out.println("Perimetro:" + cir.calcularPer());
+             System.out.println("Superficie:" + cir.calcularSup());
+             System.out.println("--------------------------------------------");
         }
-           // System.out.println("Radio:" + circulo.calcularPer());
-           // System.out.println("superficie:" + circulo.calcularSup());
-        }
-    //}    
-
-   /* public static void main(String[] args) {
         
-        ArrayList<Circulo> arrayList = new ArrayList<>();
-        int op;
+               
             
-            do{
-                System.out.println("---seleccione opcion ingresando el numero de opcion---");
-                System.out.println("1) Agregar circulo ");
-                System.out.println("2) salir");
-                op=sc.nextInt();
-                if(op==1){
-                    System.out.println("Ingrese radio ");
-                    double a = sc.nextDouble();
-                    
-                }
-                
-            }while(op==2);
-                
-            
-    }*/
-//}
+          
+        }
+    }
